1e81a250c46f23af6e9734dba4ccbc92f63612b8
cruzj <you@example.com>
2015-12-10 00:39:52 +0000

    adding some content

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,17 +2,14 @@
 
       <div class="row row-offcanvas row-offcanvas-left">
 
-	    <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+	    <div class="col-xs-4 col-sm-3 sidebar-offcanvas" id="sidebar">
 	      <div class="list-group">
-	        <a href="index.php?link=one" class="list-group-item active">Link_one</a>
-	        <a href="index.php?link=two" class="list-group-item">Link_two</a>
-	        <a href="index.php?link=three" class="list-group-item">Link_three</a>
-	        <a href="#" class="list-group-item">Link</a>
-	        <a href="#" class="list-group-item">Link</a>
-	        <a href="#" class="list-group-item">Link</a>
-	        <a href="#" class="list-group-item">Link</a>
-	        <a href="#" class="list-group-item">Link</a>
-	        <a href="#" class="list-group-item">Link</a>
-	        <a href="#" class="list-group-item">Link</a>
+	        <a href="index.php?link=one" class="list-group-item active">Brand Essence</a>
+	        <a href="index.php?link=two" class="list-group-item">Heading</a>
+	        <a href="index.php?link=three" class="list-group-item">RAVN logo</a>
+	        <a href="#" class="list-group-item">Visual Identity</a>
+	        <a href="#" class="list-group-item">Typography</a>
+	        <a href="#" class="list-group-item">Templates</a>
+	        
 	      </div>
 	    </div><!--/.sidebar-offcanvas-->
