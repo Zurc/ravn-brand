f6c5b11c33990dd30f1407fb04a48760e9c8d644
cruzj <you@example.com>
2015-12-10 00:07:37 +0000

    base structure

---
+++ application/views/index.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+include("templates/header.php");
+include("templates/sidebar.php");
+?>
+
+<div id="content">
+	<div class="col-xs-12 col-sm-9">
+	  <p class="visible-xs">
+	    <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+	  </p>
+
+	<?php 
+
+		if(empty($_GET['link'])) {
+
+			include("templates/index.php");
+
+		} else {
+			$l = $_GET['link'];
+
+			echo $l;
+		}
+		
+	?>
+
+	</div><!--/.col-xs-12.col-sm-9-->
+</div><!--/content-->
+
+<?php include("templates/footer.php"); ?>
