abc05aa6174e5e9dd8141a7ca76a9ffb4db530b5
cruzj <you@example.com>
2015-12-14 17:57:18 +0000

    adding more content to logos page

--- application/views/templates/ravn-logo.php
+++ application/views/templates/ravn-logo.php
@@ -101,3 +101,41 @@ include("nextprev.php");
 		</ul>
 	</div>
 </section>
+
+<section class="bottom-shadow">
+  <div class="col-xs-12 col-md-4">
+    <h2>Give me space...</h2>
+    <p>To ensure the RAVN logo is clearly visible in all applications, give it su cient clear space. Keep clear space free of any other visual element to maximize the recognition of RAVN’s identity.
+</p>
+	<p>When the logotype or full name is used, a clear space of 50% of the logotype’s height should be mantained.</p>
+	<p>In special circumstances when a 50% clear zone isn’t available, use the second option of a 25% clear zone.</p>
+  </div>
+  <div id="guidelines-desc"  class="col-xs-12 col-md-8">
+  	<img src="images/space.png" alt="favicon" class="rpv-img">
+	<img src="images/space2.png" alt="favicon" class="rpv-img">
+  </div>
+</section>
+
+<section class="bottom-shadow">
+  <div class="col-xs-12 col-md-4">
+    <h2>Alternate Logo Space</h2>
+    <p>In some instances where you are not able to optimally show the full RAVN logo due to space limitation, it’s better to use the RAVN alternate vertical logo.</p>
+    <p>Again, to ensure optimal visibility it’s important to give enough space around the logo.</p>
+  </div>
+  <div id="guidelines-desc"  class="col-xs-12 col-md-8 center">
+	<img src="images/alternate.png" alt="favicon" class="alternate-img">
+  </div>
+</section>
+
+<section class="bottom-shadow">
+  <div class="col-xs-12 col-md-4">
+    <h2>Minimum Size</h2>
+    <p>The RAVN logo retains its visual strength in a wide range of sizes. However, for printed and online versions the minimum size of the logo is determined by its legibility or the ability to distinguish one letter from the other.</p>
+    <p>There are no limitations for maximum size on the RAVN logo.</p>
+  </div>
+  <div id="guidelines-desc"  class="col-xs-12 col-md-8 center">
+	<img src="images/minimum.png" alt="favicon" class="rpv-img">
+  </div>
+</section>
+
+
