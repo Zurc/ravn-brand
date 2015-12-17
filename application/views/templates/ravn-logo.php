044af5cda7217226f5cf5ad9dfe133bc9e0e373a
cruzj <you@example.com>
2015-12-17 11:49:51 +0000

    adding more info on ravn logo

--- application/views/templates/ravn-logo.php
+++ application/views/templates/ravn-logo.php
@@ -13,7 +13,7 @@ include("nextprev.php");
 		<p>The preferred version stands on a white background wherever possible.</p>
 		<p>The logo should never be typeset, recreated or altered, which could cause inconsistencies.</p>
 	</div>
-	<div id="guidelines-desc"  class="col-xs-12 col-md-8">
+	<div class="col-xs-12 col-md-8">
   		<img src="images/ravn-logo-explained.png" alt="ravn-logo" class="rpv-img">
 	</div>
 </section>
@@ -110,9 +110,9 @@ include("nextprev.php");
 	<p>When the logotype or full name is used, a clear space of 50% of the logotype’s height should be mantained.</p>
 	<p>In special circumstances when a 50% clear zone isn’t available, use the second option of a 25% clear zone.</p>
   </div>
-  <div id="guidelines-desc"  class="col-xs-12 col-md-8">
-  	<img src="images/space.png" alt="favicon" class="rpv-img">
-	<img src="images/space2.png" alt="favicon" class="rpv-img">
+  <div class="col-xs-12 col-md-8">
+  	<img src="images/space.png" alt="space around logo" class="rpv-img">
+	<img src="images/space2.png" alt="space around logo" class="rpv-img">
   </div>
 </section>
 
@@ -122,8 +122,8 @@ include("nextprev.php");
     <p>In some instances where you are not able to optimally show the full RAVN logo due to space limitation, it’s better to use the RAVN alternate vertical logo.</p>
     <p>Again, to ensure optimal visibility it’s important to give enough space around the logo.</p>
   </div>
-  <div id="guidelines-desc"  class="col-xs-12 col-md-8 center">
-	<img src="images/alternate.png" alt="favicon" class="alternate-img">
+  <div class="col-xs-12 col-md-8 center">
+	<img src="images/alternate.png" alt="alternate logo space" class="alternate-img">
   </div>
 </section>
 
@@ -133,8 +133,56 @@ include("nextprev.php");
     <p>The RAVN logo retains its visual strength in a wide range of sizes. However, for printed and online versions the minimum size of the logo is determined by its legibility or the ability to distinguish one letter from the other.</p>
     <p>There are no limitations for maximum size on the RAVN logo.</p>
   </div>
-  <div id="guidelines-desc"  class="col-xs-12 col-md-8 center">
+  <div class="col-xs-12 col-md-8 center">
 	<img src="images/minimum.png" alt="favicon" class="rpv-img">
+  </div>
+</section>
+
+<section class="bottom-shadow">
+  <div class="col-xs-12 col-md-4">
+    <h2>Border Agency</h2>
+    <p>For any given format -landscape or portrait- the preferred logo placement is in any corner position or center aligned at the top or the bottom.</p>
+  </div>
+  <div class="col-xs-12 col-md-8 center">
+	<img src="images/borders_1.png" alt="border usage" class="rpv-img">
+	<img src="images/borders_2.png" alt="border usage" class="rpv-img">
+  </div>
+</section>
+
+<section class="bottom-shadow">
+  <div class="col-xs-12 col-md-4">
+    <h2>Logo Color Variation</h2>
+    <p>The RAVN logo should be reproduced in color with white background whenever possible.</p>
+    <p>If color reproduction is not available, the logo should be reproduced in Gray/Black/White.</p>
+    <p>In all cases a bird background on the symbol should remain white.</p>
+    <p>If the RAVN logo is placed on a photographic image, the image behind the logo should create enough contrast with it.</p>
+  </div>
+  <div class="col-xs-12 col-md-8 center">
+	<img src="images/logo-color-variations_1.png" alt="logo color variations" class="rpv-img">
+	<img src="images/logo-color-variations_2.png" alt="logo color variations" class="rpv-img">
+	<img src="images/logo-color-variations_3.png" alt="logo color variations" class="rpv-img">_
+  </div>
+</section>
+
+<section class="bottom-shadow">
+  <div class="col-xs-12 col-md-4">
+    <h2>Please don't...</h2>
+    <p>To keep the integrity of the RAVN logo, and to promote consistency of the brand, it is important to use the logo as described on these guidelines.</p>
+    <p>The examples shown here illustrate possible misuses of the RAVN logo that should be avoided.</p>
+  </div>
+  <div  class="col-xs-12 col-md-4 center">
+	<img src="images/dont1.png" alt="" class="rpv-img">
+	<img src="images/dont2.png" alt="" class="rpv-img">
+	<img src="images/dont3.png" alt="" class="rpv-img">
+	<img src="images/dont4.png" alt="" class="rpv-img">
+	<img src="images/dont5.png" alt="" class="rpv-img">
+  </div>
+  <div class="col-xs-12 col-md-4 center">
+	<img src="images/dont6.png" alt="" class="rpv-img">
+	<img src="images/dont7.png" alt="" class="rpv-img">
+	<img src="images/dont8.png" alt="" class="rpv-img">
+	<img src="images/dont9.png" alt="" class="rpv-img">
+	<img src="images/dont10.png" alt="" class="rpv-img">
   </div>
 </section>
 
