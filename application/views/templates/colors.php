44544d7cf0f684e9882988fd78ae4e8f7cbe743b
cruzj <you@example.com>
2015-12-21 14:44:07 +0000

    color codes updated for emerald, sapphire, and malva

--- application/views/templates/colors.php
+++ application/views/templates/colors.php
@@ -1,6 +1,7 @@
 <?php 
 
 	$colors = array( array('maroon',
+							'The RAVN main color -Maroon- used for RAVN logos and same variations on shades and tints',
 							array("#991B1E", "rgb(153, 27, 30)", "cmyk(25, 99, 99, 24)", "pantone 7628 C"),		//	main
 							array("#800205", "rgb(128, 2, 5)", "cmyk(29, 100, 100, 38)"),						//	shade 1
 							array("#4E0000", "rgb(78, 0, 0)", "cmyk(43, 89, 81, 66)"),							//	shade 2
@@ -8,11 +9,28 @@
 							array("#FF8386", "rgb(255, 131, 134)", "cmyk(0, 62, 34, 0)")						//	tint 2
 					 ),
 					 array('emerald',
+					 		'Main ACE color -Emerald- used for RAVN ACE and same variations on shades and tints.',
 					 		array("#1b9996", "rgb(27, 153, 150)", "cmyk(80, 20, 44, 1)", "pantone 7473 C"),	//	main
-							array("#02807d", "rgb(153, 27, 30)", "cmyk(25, 99, 99, 24)"),	//	shade 1
-							array("#004e4b", "rgb(153, 27, 30)", "cmyk(25, 99, 99, 24)"),	//	shade 2
-							array("#4fcdca", "rgb(153, 27, 30)", "cmyk(25, 99, 99, 24)"),	//	tint 1
-							array("#83fffe", "rgb(153, 27, 30)", "cmyk(25, 99, 99, 24)")	//	tint 2
+							array("#02807d", "rgb(2, 128, 125)", "cmyk(86, 31, 51, 9)"),					//	shade 1
+							array("#004e4b", "rgb(0, 78, 75)", "cmyk(92, 48, 63, 39)"),						//	shade 2
+							array("#4fcdca", "rgb(79, 205, 202)", "cmyk(60, 0, 26, 0)"),					//	tint 1
+							array("#83fffe", "rgb(131, 255, 254)", "cmyk(37, 0, 9, 0)"),					//	tint 2
+			  		 ),
+			  		 array('sapphire',
+					 		'Secondary color -Sapphire Blue- with variations on shades and tints.',
+					 		array("#1b5d99", "rgb(27, 93, 153)", "cmyk(93, 67, 13, 1)", "pantone 7684 C"),	//	main
+							array("#024480", "rgb(2, 68, 128)", "cmyk(100, 82, 24, 7)"),					//	shade 1
+							array("#00124e", "rgb(0, 18, 78)", "cmyk(100, 96, 31, 44)"),					//	shade 2
+							array("#4f91cd", "rgb(79, 145, 205)", "cmyk(68, 33, 0, 0)"),					//	tint 1
+							array("#83c5ff", "rgb(131, 197, 255)", "cmyk(42, 12, 0, 0)"),					//	tint 2
+			  		 ),
+			  		 array('malva',
+					 		'Secondary color -Malva- with variations on shades and tints.',
+					 		array("#961b99", "rgb(150, 27, 153)", "cmyk(50, 99, 0, 0)", "pantone 254 C"),	//	main
+							array("#7d0280", "rgb(125, 2, 128)", "cmyk(62, 100, 14, 4)"),					//	shade 1
+							array("#4b004e", "rgb(75, 0, 78)", "cmyk(72, 100, 31, 38)"),					//	shade 2
+							array("#ca4fcd", "rgb(202, 79, 205)", "cmyk(33, 77, 0, 0)"),					//	tint 1
+							array("#fe83ff", "rgb(254, 131, 255)", "cmyk(15, 52, 0, 0)"),					//	tint 2
 			  		 ),
 		);
 
@@ -22,41 +40,12 @@
 		echo '<section class="bottom-shadow">
 				<div class="col-xs-12">
 					<h2>'.$colors[$key][0].'</h2>
-					<p>The RAVN logo should be reproduced in Main color -Maroon/ Black/White with white background whenever possible.</p>
-					<p>If color reproduction is not available, the logo should be reproduced in Gray/Black/ White.</p>
-				</div>
-				<div class="col-xs-12 col-sm-2">
-					<div class="center">
-						<div class="color-box" style="background:'.$colors[$key][3][0].' "></div>
-						<p class="lead">Shade 2</p>
-					</div>
-					<p class="color-code">'.$colors[$key][3][0].'</p>
-					<p class="color-code">'.$colors[$key][3][1].'</p>
-					<p class="color-code">'.$colors[$key][3][2].'</p>
-				</div>
-				<div class="col-xs-12 col-sm-2">
-					<div class="center">
-						<div class="color-box" style="background:'.$colors[$key][2][0].' "></div>
-						<p class="lead">Shade 1</p>
-					</div>
-					<p class="color-code">'.$colors[$key][2][0].'</p>
-					<p class="color-code">'.$colors[$key][2][1].'</p>
-					<p class="color-code">'.$colors[$key][2][2].'</p>
-				</div>
-				<div class="col-xs-12 col-sm-3">
-					<div class="center">
-						<div class="main-color-box" style="background:'.$colors[$key][1][0].' "></div>
-						<p class="lead">Main</p>
-					</div>
-					<p class="color-code">'.$colors[$key][1][0].'</p>
-					<p class="color-code">'.$colors[$key][1][1].'</p>
-					<p class="color-code">'.$colors[$key][1][2].'</p>
-					<p class="color-code">'.$colors[$key][1][3].'</p>
+					<p>'.$colors[$key][1].'</p>
 				</div>
 				<div class="col-xs-12 col-sm-2">
 					<div class="center">
 						<div class="color-box" style="background:'.$colors[$key][4][0].' "></div>
-						<p class="lead">Tint 1</p>
+						<p class="lead">Shade 2</p>
 					</div>
 					<p class="color-code">'.$colors[$key][4][0].'</p>
 					<p class="color-code">'.$colors[$key][4][1].'</p>
@@ -64,12 +53,40 @@
 				</div>
 				<div class="col-xs-12 col-sm-2">
 					<div class="center">
+						<div class="color-box" style="background:'.$colors[$key][3][0].' "></div>
+						<p class="lead">Shade 1</p>
+					</div>
+					<p class="color-code">'.$colors[$key][3][0].'</p>
+					<p class="color-code">'.$colors[$key][3][1].'</p>
+					<p class="color-code">'.$colors[$key][3][2].'</p>
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<div class="center">
+						<div class="main-color-box" style="background:'.$colors[$key][2][0].' "></div>
+						<p class="lead">Main</p>
+					</div>
+					<p class="color-code">'.$colors[$key][2][0].'</p>
+					<p class="color-code">'.$colors[$key][2][1].'</p>
+					<p class="color-code">'.$colors[$key][2][2].'</p>
+					<p class="color-code">'.$colors[$key][2][3].'</p>
+				</div>
+				<div class="col-xs-12 col-sm-2">
+					<div class="center">
 						<div class="color-box" style="background:'.$colors[$key][5][0].' "></div>
-						<p class="lead">Tint 2</p>
+						<p class="lead">Tint 1</p>
 					</div>
 					<p class="color-code">'.$colors[$key][5][0].'</p>
 					<p class="color-code">'.$colors[$key][5][1].'</p>
 					<p class="color-code">'.$colors[$key][5][2].'</p>
+				</div>
+				<div class="col-xs-12 col-sm-2">
+					<div class="center">
+						<div class="color-box" style="background:'.$colors[$key][6][0].' "></div>
+						<p class="lead">Tint 2</p>
+					</div>
+					<p class="color-code">'.$colors[$key][6][0].'</p>
+					<p class="color-code">'.$colors[$key][6][1].'</p>
+					<p class="color-code">'.$colors[$key][6][2].'</p>
 				</div>
 			</section>';
 
